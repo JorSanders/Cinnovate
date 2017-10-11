2be29a4f4acc16004919ad880da3169508fdd32b
Jor Sanders <jorsanders@hotmail.com>
2017-10-11 09:40:24 +0200

    small update to the pozyx class

--- App1/Models/Pozyx.cs
+++ App1/Models/Pozyx.cs
@@ -30,38 +30,30 @@ namespace App1.Models
             _PozyxShield = await I2cDevice.FromIdAsync(devices[0].Id, Pozyx_settings);
         }
 
-        private void DecodeByte(string ByteInputted)
+        private int[] DecodeByte(byte ByteInputted)
         {
-            byte result = 0x23;
             byte versionPart = 0x1f;
             byte typePart = 0xE0;
-            UInt16 version = (byte)(result & versionPart);
-            byte typeShifted = (byte)(result & typePart);
+            UInt16 version = (byte)(ByteInputted & versionPart);
+            byte typeShifted = (byte)(ByteInputted & typePart);
             UInt16 type = (byte)(typeShifted >> 5);
+
+            int[] result = { version, type };
+
+            return result;
         }
 
-        private void request(byte sendByte)
+        private byte[] request(byte sendByte, int lenght)
         {
-            byte code = Convert.ToByte(TextBox1.Text, 16);
-            int bits = Int32.Parse(TextBox2.Text);
-
             // Read data from I2C.
-            var command = new byte[1];
-            var data = new byte[bits];
+            byte[] command = new byte[1];
+            byte[] result = new byte[lenght];
 
-            command[0] = code;
+            byte[] sendByteArray = { sendByte };
 
-            _PozyxShield.WriteRead(command, data);
+            _PozyxShield.WriteRead(sendByteArray, result);
 
-            string str = String.Empty;
-            for (int i = 0; i < data.Length; i++)
-            {
-                str = str + "/ " + data[i].ToString() + " ";
-            }
-
-            TextBlock1.Text = "Result: " + str;
-            //TextBlock2.Text = "String: " + System.Text.Encoding.UTF8.GetString(data);
-
+            return result;
         }
 
     }
