ae34a7dac8531a7ddca85d6f50d5af580c5b7cf3
Jor Sanders <jorsanders@hotmail.com>
2017-10-12 15:54:35 +0200

    get anchor id finished but void for now

--- App1/Models/Pozyx.cs
+++ App1/Models/Pozyx.cs
@@ -166,7 +166,7 @@ namespace App1.Models
 
         public void GetAnchorPosition(byte[] anchorId)
         {
-            byte[] request = { 0xB6, anchorId[0], anchorId[1] };
+            byte[] request = { 0xC6, anchorId[0], anchorId[1] };
             byte[] data = Request(request, 13);
 
             if (data.Length <= 0 || data[0] != 1)
@@ -176,8 +176,14 @@ namespace App1.Models
 
 
             byte[] xBytes = { data[1], data[2], data[3], data[4] };
+            byte[] yBytes = { data[5], data[6], data[7], data[8] };
+            byte[] zBytes = { data[9], data[10], data[11], data[12] };
+
 
             int x = BitConverter.ToInt32(xBytes, 0);
+            int y = BitConverter.ToInt32(yBytes, 0);
+            int z = BitConverter.ToInt32(zBytes, 0);
+
 
 
         }
