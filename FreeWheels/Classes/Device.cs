425511fc6f15bf8d04abde0f9612d70bb366d909
Dobbler <dobbler@info.com>
2017-11-07 14:24:37 +0100

    #135: Ready For TeamTest

--- FreeWheels/Classes/Device.cs
+++ FreeWheels/Classes/Device.cs
@@ -21,42 +21,6 @@ namespace FreeWheels.Classes
             this.NetworkID = networkID;
         }
 
-        public void GetDeviceInfo()
-        {
-            DeviceInfo deviceInfo = PozyxApi.DeviceGetInfo(this.NetworkID);
-            this.Flag = deviceInfo.Flag;
-            this.X = deviceInfo.X;
-            this.Y = deviceInfo.Y;
-            this.Z = deviceInfo.Z;
-        }
-
-    }
-
-    public class DeviceInfo
-    {
-
-        public int Flag { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Z { get; set; }
-
-        public DeviceInfo() { }
-
-        public DeviceInfo(int x, int y, int z)
-        {
-            this.X = x;
-            this.Y = y;
-            this.Z = z;
-        }
-
-        public DeviceInfo(int flag, int x, int y, int z)
-        {
-            this.Flag = flag;
-            this.X = x;
-            this.Y = y;
-            this.Z = z;
-        }
-
     }
 
 }
