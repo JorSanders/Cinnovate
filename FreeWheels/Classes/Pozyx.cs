03c51bfefd23ccb4aa8f53efa187073cad9ba447
Dobbler <dobbler@info.com>
2017-11-09 13:55:24 +0100

    Clean sheet

--- FreeWheels/Classes/Pozyx.cs
+++ FreeWheels/Classes/Pozyx.cs
@@ -12,56 +12,9 @@ namespace FreeWheels.Classes
     public class Pozyx
     {
 
-        public List<Anchor> Anchors;
-        public Tag MyPozyx;
-
         public Pozyx()
         {
             Connection.Connect();
-            MyPozyx = new Tag();
-        }
-
-        public bool Init()
-        {
-            if (!PozyxApiBase.DiscoverDevices())
-            {
-                return false;
-            }
-
-            if (!PozyxApiBase.StartPositioning())
-            {
-                return false;
-            }
-
-            if (!PozyxApiBase.CalibrateDevices())
-            {
-                return false;
-            }
-
-            List<byte[]> anchorIds = PozyxApiBase.GetAnchorIds();
-
-            Anchors = new List<Anchor>();
-
-            foreach (byte[] anchorId in anchorIds)
-            {
-                Anchors.Add(new Anchor(anchorId));
-            }
-
-            foreach (Anchor anchor in Anchors)
-            {
-                anchor.Position = PozyxApiBase.GetAnchorPosition(anchor.Id);
-            }
-
-            MyPozyx = new Tag();
-
-            return true;
-        }
-
-        public bool Reset()
-        {
-            Anchors = new List<Anchor>();
-            MyPozyx = new Tag();
-            return PozyxApiBase.Reset();
         }
 
         public bool LetsGo()
