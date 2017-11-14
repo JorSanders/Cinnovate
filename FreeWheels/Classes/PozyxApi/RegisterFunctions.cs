728227f9dbf7b7baad15ff6548297710e76d5558
Marco Prins <mk-prins@live.nl>
2017-11-14 09:46:02 +0100

    Feature/#144 PozyxApiBase Refactor (#10)
    * #144 Refactor PozyxApiBase + Status Registers
    
    * #144: Added missing comment to DevicesCalibrate()
    
    * Hotfix
    
    * Clean sheet

--- FreeWheels/Classes/PozyxApi/RegisterFunctions.cs
+++ FreeWheels/Classes/PozyxApi/RegisterFunctions.cs
@@ -8,7 +8,20 @@ namespace FreeWheels.Classes.PozyxApi
 {
     public static class RegisterFunctions
     {
-        
+
+        /// <summary>
+        ///     Calling this function resets the Pozyx device.
+        ///     This also clears the device list and returns the settings to their defualt state (including UWB settings)
+        /// </summary>
+        /// <returns></returns>
+        public static bool ResetSys()
+        {
+            byte[] request = { 0xB0 };
+            byte[] data = Connection.ReadWrite(request, 1);
+
+            return data[0] == 1;
+        }
+
         public static bool TXData(int offset, byte[] dataBytes)
         {
             byte[] request = new byte[dataBytes.Length + 2];
@@ -33,6 +46,33 @@ namespace FreeWheels.Classes.PozyxApi
         public static void RXData(byte[] dataBytes)
         {
 
+        }
+
+        public static bool DoPositioning()
+        {
+            byte[] request = { 0xB6 };
+            byte[] data = Connection.ReadWrite(request, 1);
+
+            return data[0] == 1;
+        }
+
+        public static List<int> GetAnchorIds()
+        {
+            byte[] request = { 0xB8 };
+            byte[] data = Connection.ReadWrite(request, 33);
+
+            List<int> anchorIds = new List<int>();
+
+            for (int i = 1; i + 1 < data.Length; i += 2)
+            {
+                if (!(data[i] == 0 && data[i + 1] == 0))
+                {
+                    int id = BitConverter.ToUInt16(new byte[] { data[i], data[i + 1] }, 0);
+                    anchorIds.Add(id);
+                }
+            }
+
+            return anchorIds;
         }
 
     }
