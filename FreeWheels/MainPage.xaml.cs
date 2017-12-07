32187d1c7ed43d117d3b514a60b03a292fc8a7c3
Nadim Ammor <nadimammor@gmail.com>
2017-12-07 13:44:28 +0100

    Finished tracking

--- FreeWheels/MainPage.xaml.cs
+++ FreeWheels/MainPage.xaml.cs
@@ -183,43 +183,15 @@ namespace FreeWheels
             args.DrawingSession.FillCircle((float)(_MyPosition.Y * pixelSize + space), (float)(_MyPosition.X * pixelSize + space), 5, Colors.Green);
             */
 
-            //lines 
-            //var lastPosition = this._MyPosition.X * pixelSize + space;
+            //Adds the route walked to the map
 
-            //positions.Add(this._MyPosition.X * pixelSize + space);
+            posX.Add(this._MyPosition.X * pixelSize + space);
+            posY.Add(this._MyPosition.Y * pixelSize + space);
 
-            //args.DrawingSession.DrawLine((float)lastPosition + 100, (float)currentPosition + 5, (float)lastPosition + 100, (float)currentPosition + 5, Colors.Yellow);
-
-            var currentPositionX = this._MyPosition.X * pixelSize + space;
-            var currentPositionY = this._MyPosition.Y * pixelSize + space;
-
-            var polyline1 = new Polyline();
-            polyline1.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
-            polyline1.StrokeThickness = 4;
-
-            startingPositionX = currentPositionX;
-            startingPositionY = currentPositionY;
-
-            posX.Add(currentPositionX);
-            posY.Add(currentPositionY);
-
-
-            var points = new PointCollection();
-            points.Add(new Windows.Foundation.Point(posX[i], posY[i]));
-            points.Add(new Windows.Foundation.Point(posX[i], posY[i]));
-            polyline1.Points = points;
-
-            for (int i = 0; i < posY.Count; i++) {
-
-                this.Output.Text = posY[i].ToString();
-
+            for (int i = 0; i < posX.Count - 1; i++)
+            {
+                args.DrawingSession.DrawLine((float)posX[i], (float)posY[i], (float)posX[i + 1], (float)posY[i + 1], Windows.UI.Colors.Red, 1);
             }
-
-            //_MyPosition = _Pozyx.PositioningData.Pos();
-            //_MyPosition.X.ToString();
-            
-            layoutRoot.Children.Add(polyline1);
-
 
 
         }
@@ -267,7 +239,7 @@ namespace FreeWheels
                     dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 50);
 
                     GridCanvas.Visibility = Visibility.Visible;
-                   
+
                 }
 
                 dispatcherTimer.Start();
@@ -430,7 +402,7 @@ namespace FreeWheels
             polyline1.StrokeThickness = 4;
 
             var points = new PointCollection();
-            points.Add(new Windows.Foundation.Point(1,10));
+            points.Add(new Windows.Foundation.Point(1, 10));
             points.Add(new Windows.Foundation.Point(200, 500));
             polyline1.Points = points;
 
